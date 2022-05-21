33ce9d3028cfe3d554d71601e16596332a2a840f
huthoinguyn <huthoi99@gmail.com>
2022-05-21 19:22:59 +0700

    add content #2 blog

--- main.js
+++ main.js
@@ -41,4 +41,32 @@ window.addEventListener("load", function () {
       `;
     dishList.insertAdjacentHTML("beforeend", template);
   });
+
+  const blogItem = [
+    {
+      img: "./images/blog-thumbnail-image-1.jpg",
+      title: "The Legend Of US Cuisine: The Story Of ",
+      desc: "Capitalize on low-hanging fruit to identify a ballpark value added matrix economically and the creative activity to beta test override the food quality.",
+    },
+    {
+      img: "./images/blog-thumbnail-image-2.jpg",
+      title: "The Most Popular Delicious Food Of Mediterranean Cuisine ",
+      desc: "Strategies on low-hanging fruit to identify a ballpark value added matrix economically and the creative activity to beta test override the food quality.",
+    },
+  ];
+  const blogList = document.querySelector(".blog-list");
+  blogItem.map((item) => {
+    const blogTemplate = `
+  <div class="blog-item">
+  <div class="blog-img">
+      <img src="${item.img}" alt="">
+  </div>
+  <div class="blog-content">
+      <h2 class="title">${item.title}</h2>
+      <p class="desc">${item.desc}</p>
+      </div>
+  </div>
+  `;
+    blogList.insertAdjacentHTML("beforeend", blogTemplate);
+  });
 });
